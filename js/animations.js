42059b0c262ff00b2ff8709f28723b039770d96e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-04-26 17:57:56 +0300

    Add image clippath animation

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,18 @@
+function animateImagesClipPath(selector) {
+  const items = document.querySelectorAll(selector)
+  items.forEach((item) => {
+    const tl = new gsap.timeline({ scrollTrigger: item })
+    tl.fromTo(
+      item,
+      { webkitClipPath: 'polygon(0 0, 0% 0, 0% 0%, 0% 0%)' },
+      { webkitClipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }
+    )
+  })
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
   window.addEventListener('load', function () {
     gsap.registerPlugin(ScrollTrigger)
+    animateImagesClipPath('.animate-image')
   })
 })
