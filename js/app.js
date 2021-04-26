55824387f5dd706123f052bc6ffb57fc75cfe13e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-04-26 17:11:50 +0300

    Add loader and page transition animation with barbajs

--- js/app.js
+++ js/app.js
@@ -1,30 +1,49 @@
+function delay(n) {
+  n = n || 2000
+  return new Promise((done) => {
+    setTimeout(() => {
+      done()
+    }, n)
+  })
+}
+
+function controlLinkEvents() {
+  var links = document.querySelectorAll('a[href]')
+  var cbk = function (e) {
+    if (e.currentTarget.href === window.location.href) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+  }
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', cbk)
+  }
+}
+
+function pageTransition() {
+  const tl1 = gsap.timeline()
+  tl1.to('ul.preload-boxes li', { duration: 0.4, ease: 'expo.in', scaleY: 1, stagger: 0.12 })
+  tl1.to('ul.preload-boxes li', { duration: 0.4, ease: 'expo.out', scaleY: 0, stagger: 0.08 })
+}
+
+function loaderTransition() {
+  const tl1 = gsap.timeline()
+  tl1.to('ul.preload-boxes li', { duration: 0.6, ease: 'expo.in', scaleY: 0, stagger: 0.05 })
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
-  window.addEventListener('load', function () {
-    gsap.registerPlugin(ScrollTrigger)
-
-    // const tl1 = gsap.timeline();
-    // tl1.to(".box", {
-    //   scale: 2,
-    //   duration: 1.3,
-    //   delay: 0.3
-    // });
-
-    const customCursor = document.getElementById('custom-cursor')
-    const cursorPoint = document.getElementById('point')
-    document.addEventListener('mousemove', (e) => {
-      const cursorX = e.x
-      const cursorY = e.y
-      // customCursor.style.transform = `translate(${cursorX - 20}px, ${cursorY - 20}px)`
-      customCursor.style.top = `${cursorY - 20}px`
-      customCursor.style.left = `${cursorX - 20}px`
-    })
-    document.addEventListener('click', () => {
-      const customCursorClassList = [...customCursor.classList]
-      customCursor.classList.add('click')
-      setTimeout(() => {
-        // customCursorClassList.includes('click') ? customCursor.classList.remove('click') : undefined
-        customCursor.classList.remove('click')
-      }, 300)
+  window.addEventListener('load', async function () {
+    controlLinkEvents()
+    loaderTransition()
+    barba.init({
+      transitions: [
+        {
+          async leave(data) {
+            pageTransition()
+          },
+        },
+      ],
     })
   })
 })
