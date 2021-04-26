0f21483d0b2603328a81a86e0177d84bcb1f69cf
Kaan Ersoy <kaanersoy12@gmail.com>
2021-04-26 13:23:52 +0300

    Add custom-cursor

--- js/app.js
+++ js/app.js
@@ -1,43 +1,30 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  window.addEventListener("load", function () {
-    gsap.registerPlugin(ScrollTrigger);
-    const tl1 = gsap.timeline();
-    tl1.to(".box", {
-      scale: 2,
-      duration: 1.3,
-      delay: 0.3
-    });
-    tl1.from(
-      "#headline1",
-      {
-        y: 250
-      },
-      "-=0.5"
-    );
-    tl1.from(
-      "#headline2",
-      {
-        y: 250
-      },
-      "-=0.3"
-    );
-    tl1.from(".underline", {
-      y: 250
-    });
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('load', function () {
+    gsap.registerPlugin(ScrollTrigger)
 
-    let container = document.getElementById("viewWrapper");
+    // const tl1 = gsap.timeline();
+    // tl1.to(".box", {
+    //   scale: 2,
+    //   duration: 1.3,
+    //   delay: 0.3
+    // });
 
-    gsap.to(container, {
-      x: () =>
-        -(container.scrollWidth - document.documentElement.clientWidth) + "px",
-      ease: "none",
-      scrollTrigger: {
-        trigger: container,
-        invalidateOnRefresh: true,
-        pin: true,
-        scrub: 1,
-        end: () => "+=" + container.offsetWidth
-      }
-    });
-  });
-});
+    const customCursor = document.getElementById('custom-cursor')
+    const cursorPoint = document.getElementById('point')
+    document.addEventListener('mousemove', (e) => {
+      const cursorX = e.x
+      const cursorY = e.y
+      // customCursor.style.transform = `translate(${cursorX - 20}px, ${cursorY - 20}px)`
+      customCursor.style.top = `${cursorY - 20}px`
+      customCursor.style.left = `${cursorX - 20}px`
+    })
+    document.addEventListener('click', () => {
+      const customCursorClassList = [...customCursor.classList]
+      customCursor.classList.add('click')
+      setTimeout(() => {
+        // customCursorClassList.includes('click') ? customCursor.classList.remove('click') : undefined
+        customCursor.classList.remove('click')
+      }, 300)
+    })
+  })
+})
